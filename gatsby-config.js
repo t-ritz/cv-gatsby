070fb23e93f06913c05d094788294dcd7a7c8ab1
Thibaud <thibaud.ritzenthaler@pm.me>
2022-02-24 20:23:50 +0100

    Git hub pages compatibility 🛸

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,33 @@
 module.exports = {
   siteMetadata: {
-      title: `CV Thibaud Ritzenthaler`,
+    title: `CV Thibaud Ritzenthaler`,
     siteUrl: `https://thibaudritzenthaler.dev`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }],
-  pathPrefix: "/cv",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }],
+  pathPrefix: "/cv-gatsby",
 };
